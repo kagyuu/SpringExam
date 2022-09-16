298d18a7877ca1797f78f75d1f3196684782a4b8
atsushihondoh <atsushi.hondoh@gmail.com>
2022-09-17 00:00:33 +0900

    Fix for packaging build.

--- src/test/java/com/example/springexam/SpringExamApplicationTests.java
+++ src/test/java/com/example/springexam/SpringExamApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springexam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringExamApplication.class)
 class SpringExamApplicationTests {
 
 	@Test
